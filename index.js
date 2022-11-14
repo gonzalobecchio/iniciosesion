f852e07e76db405b2fca892ae2b0d1dad0382fcb
Gonzalo Becchio <gonzalobecchio@gmail.com>
2022-11-14 17:28:33 -0300

    Subiendo Comando.txt y archivo configuracion nginx.conf

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ const { Router } = express
 
 const { fork } = require('child_process')
 
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
-const testAsync =  Router()
 
-const options = {default: {port: 8080}, alias: {p: 'port'}}
-// const args = require('minimist')(process.argv.slice(2), options)
-const args = require('minimist')(process.argv.slice(2))
+const testAsync = Router()
+
+const options = { default: { port: 8080, modo: 'fork' }, alias: { p: 'port', m: 'modo' } }
+const args = require('minimist')(process.argv.slice(2), options)
+// const args = require('minimist')(process.argv.slice(2))
 
 mongoose.connect(process.env.MONGO_URI)
 
-// const PORT = 3000
+const PORT = args.port 
+const MODO = args.modo
+
 
 const User = mongoose.model('User', new mongoose.Schema({
     email: {
@@ -36,7 +41,7 @@ const User = mongoose.model('User', new mongoose.Schema({
         type: "String",
         // required: true
     }
-    
+
 }))
 
 
@@ -51,7 +56,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
 
 app.use(session({
-    secret : process.env.MY_SECRET,
+    secret: process.env.MY_SECRET,
     cookie: {
         httpOnly: false,
         secure: false,
@@ -69,25 +74,25 @@ app.use(passport.session())
 passport.use('login', new LocalStrategy({
     usernameField: 'email'
 },
-async (email, password, done) => {
-    const userFound = await User.findOne({email}).exec()
-    
-    // console.log(userFound)
-    if (!userFound) {
-        console.log(`User not found ${email}`)
-        return done(null, false)
+    async (email, password, done) => {
+        const userFound = await User.findOne({ email }).exec()
+
+        // console.log(userFound)
+        if (!userFound) {
+            console.log(`User not found ${email}`)
+            return done(null, false)
+        }
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        // console.log(isMatch)
+
+        if (!isMatch) {
+            console.log('Invalid Password!')
+            return done(null, false)
+        }
+
+        return done(null, userFound)
     }
-    
-    const isMatch = await bcrypt.compare(password, userFound.password)
-    // console.log(isMatch)
-    
-    if (!isMatch) {
-        console.log('Invalid Password!')
-        return done(null, false)
-    }
-    
-    return done(null, userFound)
-}
 ))
 
 
@@ -98,18 +103,18 @@ passport.use('register', new LocalStrategy({
     if (!email || !password) {
         done(null, false)
     }
-    const userFound = await User.findOne({email}).exec()
+    const userFound = await User.findOne({ email }).exec()
     // console.log(userFound)
     if (userFound) {
         console.log(`Usuario ${userFound.email} already exists`)
         return done(null, false)
     }
-    
+
     try {
         const salt = await bcrypt.genSalt()
-		const hash = await bcrypt.hash(password, salt)
-        
-        const newUser = new User({email, password: hash})
+        const hash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({ email, password: hash })
         newUser.save()
         console.log('User created succeful')
         done(null, newUser)
@@ -128,10 +133,9 @@ passport.deserializeUser(async function (_id, done) {
     done(null, user);
 });
 
-app.get('/' , (req, res) => {
-    // console.log(req)
+app.get('/', (req, res) => {
     if (!req.isAuthenticated()) {
-        res.render('login')
+        res.render('login', {'port': PORT, 'pid': process.pid})
         return
     }
     const user = req.user
@@ -143,27 +147,27 @@ app.get('/' , (req, res) => {
     })
 })
 
-app.get('/register' , (req, res) => {
+app.get('/register', (req, res) => {
     if (!req.isAuthenticated()) {
         res.render('register')
         return
     }
-    
+
     const user = req.user
     res.render('profile', {
         'user': [
             user.email
-        ]  
+        ]
     })
 
 })
 
-app.post('/login', passport.authenticate('login', {failureRedirect: '/failedAuth'}) , (req, res) => {
+app.post('/login', passport.authenticate('login', { failureRedirect: '/failedAuth' }), (req, res) => {
     const user = req.user
     res.render('profile', {
         'user': [
             user.email
-        ]  
+        ]
     })
 })
 
@@ -185,13 +189,13 @@ app.get('/profile', (req, res) => {
         res.render('login')
         return
     }
-    
+
     // console.log(req.user)
     const user = req.user
     res.render('profile', {
         'user': [
             user.email
-        ]  
+        ]
     })
 })
 
@@ -205,37 +209,65 @@ app.get('/failedAuth', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    const p =  args.p 
+    const p = args.p
+    console.log(PORT)
     res.send({
-        argInput : { p },
-        SO : process.platform,
+        argInput: { p },
+        SO: process.platform,
         vNode: process.version,
         rss: process.memoryUsage,
         pathExc: process.execPath,
         pID: process.pid,
-        directory: process.cwd()
-        
+        directory: process.cwd(),
+        procesadores: numCPUs,
     })
 })
 
 
 testAsync.get('/randoms', (req, res) => {
-    let { cant } = req.query 
+    let { cant } = req.query
     if (!cant) {
         cant = 100000000
     }
-    
+
     const forked = fork('./child.js')
-    
+
     forked.send(cant)
-    
+
     forked.on('message', unicos => {
-        console.log(unicos)
+        console.log(PORT)
         res.send(unicos)
+        // res.render('register', {'port': PORT})
     })
+    // res.send(PORT)
 })
 
 app.use('/api', testAsync)
 
+// console.log(MODO)
+// console.log(PORT)
 
-app.listen(args.p, () => { console.log(`Corriendo en Puerto ${args.p}`)})
+if (MODO == 'cluster') {
+
+    if (cluster.isMaster) {
+        console.log(`PID MASTER ${process.pid}`)
+
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork()
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Worker ${worker.process.pid} died`)
+        })
+    } else {
+        app.listen(PORT, () => { console.log(`Corriendo en Puerto ${PORT}`) })
+        console.log(`Worker ${process.pid} started`)
+    }
+} else {
+    app.listen(PORT, () => { console.log(`Corriendo en Puerto ${PORT}`) })
+    console.log(`Process ${process.pid} started`)
+    
+}
+
+// app.listen(PORT, () => { console.log(`Corriendo en Puerto ${PORT}`) })
+
